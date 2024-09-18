03e232e4f57172c6206d5f0044b7c354fbf007a6
Novjean J Kannathara <novjeanj@gmail.com>
2024-09-18 19:13:39 +0530

    ui and test case

---
+++ app/src/test/java/com/novatech/cleanarchvalidation/domain/usecase/ValidatePasswordTest.kt
@@ -0,0 +1,22 @@
+package com.novatech.cleanarchvalidation.domain.usecase
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class ValidatePasswordTest {
+
+    private lateinit var validatePassword: ValidatePassword
+
+    @Before
+    fun setUp() {
+        validatePassword = ValidatePassword()
+    }
+
+    @Test
+    fun `Password is letter-only, return error`() {
+        val result = validatePassword.execute("abcdefgh")
+        assertEquals(result.successful, false)
+    }
+}
